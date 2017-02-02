3ed922bb76bb0b92873df6b50e8aecf3c0b03a7a
psato <forhighlow@gmail.com>
2017-02-02 11:22:59 +0900

    マージコピペ

--- test0117/CameraViewController.swift
+++ test0117/CameraViewController.swift
@@ -222,3 +222,74 @@ class CameraViewController: UIViewController, UIGestureRecognizerDelegate {
     }
     
 }
+
+class ViewController: UIViewController {
+    
+    // file操作をするNSFileManager
+    // 録音したファイルをDocumentsディレクトリに保存
+    // 端末のDocumentsディレクトリに保存された情報を再生画面で読み込む.
+    // 機能的に独立しているため,一つの案として，カメラと録音のアプリを独立させ，機能を起動させるボタンを押すと他のアプリケーションが起動する，というのはどうだろうか．
+    // あるいは，実機のアプリケーション"写真"の中に入っている場合，そこから時間軸からノートの中に埋め込む
+    let fileManager = NSFileManager()
+    var audioRecorder: AVAudioRecorder?
+    var audioPlayer: AVAudioPlayer?
+    let fileName = "sample.caf"
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupAudioRecorder()
+    }
+    
+    // 録音ボタンを押した時の挙動
+    @IBAction func pushRecordButton(sender: AnyObject) {
+        audioRecorder?.record()
+    }
+    
+    // 再生ボタンを押した時の挙動
+    @IBAction func pushPlayButton(sender: AnyObject) {
+        self.play()
+    }
+    
+    // 録音するために必要な設定を行う
+    // viewDidLoad時に行う
+    func setupAudioRecorder() {
+        // 再生と録音機能をアクティブにする
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        try! session.setActive(true)
+        let recordSetting : [String : AnyObject] = [
+            AVEncoderAudioQualityKey : AVAudioQuality.Min.rawValue,
+            AVEncoderBitRateKey : 16,
+            AVNumberOfChannelsKey: 2,
+            AVSampleRateKey: 44100.0
+        ]
+        do {
+            try audioRecorder = AVAudioRecorder(URL: self.documentFilePath(), settings: recordSetting)
+        } catch {
+            print("初期設定でerror出た")
+        }
+    }
+    // 再生
+    func play() {
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: self.documentFilePath())
+        } catch {
+            print("再生時にerror出た")
+        }
+        audioPlayer?.play()
+        
+    }
+    // 録音するファイルのパスを取得(録音時、再生時に参照)
+    func documentFilePath()-> NSURL {
+        let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask) as [NSURL]
+        let dirURL = urls[0]
+        return dirURL.URLByAppendingPathComponent(fileName)
+    }
+    
+    
+}
+
+
